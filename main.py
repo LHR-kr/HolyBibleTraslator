23876606b4c9d0870dd09ff82e920f9f7b80b1fe
LeeHaeRang <doco7575@gmail.com>
2022-05-24 00:49:49 +0900

    번역해야 하는 문장 성분 선별

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import konlpy
 
-# Press the green button in the gutter to run the script.
+dict_translator = {
+    "VCP" : "이니라", # 긍정 지정사
+    "VCN" : "아니하다", # 부정 지정사
+    "IC" : "아아", # 감탄사
+    "EFN" : "",#평서형 종결어미
+    "EFQ":"", # 의문형 종결어미
+    "EFO":"", #명령형 종결어미
+    "EFA":"", #청유형 종결어미
+    "EFI":"", # 감탄형 종결어미
+    "EFR":"", # 존칭형 종결 어미
+
+}
+
+
 if __name__ == '__main__':
     inputText = input("한글 문장을 입려하여 주세요.\n")
     posTaggier = konlpy.tag.Kkma()
